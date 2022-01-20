7af25b93f47d074296de7d4ec25c98ea9aae7b6b
bjoerni79 <bhillebrand79@gmail.com>
2022-01-20 13:32:22 +0100

    Added UI and view model for a Base64 helper dialog

---
+++ DevNotePad/Shared/Base64ToolDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DevNotePad.Shared
+{
+    /// <summary>
+    /// Interaction logic for Base64ToolDialog.xaml
+    /// </summary>
+    public partial class Base64ToolDialog : Window
+    {
+        public Base64ToolDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
