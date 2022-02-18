0872ed2d689c9536bbadf5f2cdde3621503b64d7
bjoerni79 <bhillebrand79@gmail.com>
2022-02-18 11:50:44 +0100

    Created a test project for the features

---
+++ DevNotPad.Features.Test/XML/BasicXmlComponentTest.cs
@@ -0,0 +1,29 @@
+﻿using DevNotePad.Features;
+using DevNotePad.Features.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevNotPad.Features.Test.XML
+{
+    [TestClass]
+    public class BasicXmlComponentTest
+    {
+        private IXmlComponent xmlComponent;
+
+        [TestInitialize]
+        public void Init()
+        {
+            xmlComponent = FeatureFactory.CreateXml();
+        }
+
+        [TestMethod]
+        public void PlaygroundTest()
+        {
+
+        }
+    }
+}
