4e2abe19f25dc31a13ac25032ab624d406abf565
Björn <bhillebrand79@gmail.com>
2022-05-14 17:11:46 +0200

    First version of the build script and installer

--- installer/build.ps1
+++ installer/build.ps1
@@ -7,14 +7,14 @@ function Run-Candle
     #Invoke-Command -FilePath $exe -ArgumentList -v $source
     #..\wix\bin\candle.exe -v installer.wxs
 
-    & $pathToWix\Candle.exe -v $source -o work\installer.wixobj 
+    & $pathToWix\Candle.exe $source -o work\installer.wixobj 
 }
 
 function  Run-Light 
 {
     Write-Output "Calling light..."
 
-    & $pathToWix\light.exe -v work\installer.wixobj
+    & $pathToWix\light.exe work\installer.wixobj
 }
 
 function Prepare
@@ -22,10 +22,14 @@ function Prepare
     # Remove it if it already exists
     if (Test-Path work)
     {
-        Remove-Item -force work 
+        Remove-Item -Force -Recurse work 
     }
 
     New-Item -Name work -ItemType "directory"
+}
+
+function CleanUp{
+    Remove-Item *.wixpdb
 }
 
 ## Main
@@ -33,9 +37,8 @@ Write-Output "Building the installer"
 Write-Output "Path to Wix binaries : $pathToWix"
 Write-Output "Source File : $source"
 
-
-
 # Build the installer
 Prepare
 Run-Candle
 Run-Light
+CleanUp
